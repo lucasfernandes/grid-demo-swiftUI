14d941b4e7c85075eb33090bc398cba4b3526490
Lucas Silveira <lucas.fernandes.silveira@gmail.com>
2019-07-13 21:18:47 -0300

    chunked grid

--- Grid-Demo/ContentView.swift
+++ Grid-Demo/ContentView.swift
@@ -9,15 +9,31 @@
 import SwiftUI
 
 struct ContentView : View {
-    var body: some View {
-        Text("Hello World")
+  
+  let dishes = Dish.all()
+  
+  var body: some View {
+    
+    let chunkedDishes = dishes.chunked(into: 2)
+    
+    return List {
+      ForEach(0..<chunkedDishes.count) { index in
+        HStack {
+          ForEach(chunkedDishes[index]) { dish in
+            Image(dish.imageURL)
+              .resizable()
+              .scaledToFit()
+          }
+        }
+      }
     }
+  }
 }
 
 #if DEBUG
 struct ContentView_Previews : PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
 #endif
